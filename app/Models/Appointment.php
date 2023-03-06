a18d5df9a0c16e3052206646f2bf53280bd5c500
kmatagua <kmatagua@gmail.com>
2023-03-06 15:48:26 -0500

    Update Appointment.php

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Appointment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'scheduled_date',
+        'scheduled_time',
+        'type',
+        'description',
+        'doctor_id',
+        'patient_id',
+        'specialty_id'
+    ];
 }
